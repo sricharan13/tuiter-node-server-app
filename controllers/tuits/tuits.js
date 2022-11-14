8331ec034c13fd04525b4618894a7e10238c874f
sricharan13 <sricharanvemuri6@gmail.com>
2022-11-14 13:33:30 -0500

    a8 fall 2022

---
+++ controllers/tuits/tuits.js
@@ -0,0 +1,33 @@
+export default [
+    { "_id": 123,
+        "topic": "Cybertruck",
+        "username": "Tesla",
+        "title": "Tesla Cybertruck lands on Mars and picks up the Curiosity rover on its 6' bed",
+        "time": "2h",
+        "image": "tesla.jpeg",
+        "liked": true,
+        "replies": 345,
+        "retuits": 321,
+        "likes": 1234,
+        "dislikes": 124,
+        "disliked": false,
+        "handle": "@tesla",
+        "tuit": "Tesla Cybertruck lands on Mars and picks up the Curiosity rover on its 6' bed. Cybertruck is built with an exterior shell made for ultimate durability and passenger protection.\nStarting withra nearly impenetrable exoskeleton, every\ncomponent is designed for superior strength and endurance, from Ultra-Hard 30X Cold-Rolled stainless-steel structural skin to Tesla armor glass."
+    },
+    {
+        "_id": 234,
+        "topic": "Space",
+        "username": "SpaceX",
+        "time": "2h",
+        "title": "100s of SpaceX Starships land on Mars after a 6 month journey. 1000s of Martian colonists being building Mars Base 1",
+        "image": "spacex.jpeg",
+        "liked": true,
+        "replies": 123,
+        "retuits": 432,
+        "likes": 2345,
+        "dislikes": 1000,
+        "disliked": true,
+        "handle": "@spacex",
+        "tuit": "You want to wake up in the morning and think the future is going to be great - and that’s what being a spacefaring civilization is all about. It’s about believing in the future and thinking that the future will be better than the past. And I can’t think of anything more exciting than going out there and being among the stars"
+    },
+];
